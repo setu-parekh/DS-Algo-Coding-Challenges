1dfe5551e5035ef0f8576f4b8de417174426579c
setu-parekh <setuparekh@outlook.com>
2019-12-08 23:04:14 -0600

    Educative: find_second_max_value_in_a_list

--- Array_String_Hash_Stack_Queue/Educative/find_second_max_value_in_a_list.py
+++ Array_String_Hash_Stack_Queue/Educative/find_second_max_value_in_a_list.py
@@ -16,8 +16,8 @@ APPROACH 1 - With Sorting
 
     TIME COMPLEXITY - O(NlogN)
 
-APPROACH 2 - Without Sorting
------------------------------
+APPROACH 2 - Without Sorting (Double Traversal)
+------------------------------------------------
     1. Initialize max_num and second_max_num to lowest number possible.
     2. Iterate through the list to find the maximum number.
     3. Iterate through the list 2nd time:
@@ -27,7 +27,18 @@ APPROACH 2 - Without Sorting
 
     TIME COMPLEXITY - O(N)
 
+APPROACH 3 - Without Sorting (Single Traversal)
+------------------------------------------------
+    1. Initialize max_num and second_max_num to lowest number possible.
+    2. Iterate through the list:
+        i. if the element is greater than the max_num:
+            - then update the max_num = element
+            - update the second_max_num = max_num
+        ii. if the element is smaller than the max_num:
+            - check whether the element is greater than the second_max_num and not equal to the max_num.
+                - If yes, then update the second_max_num.
 
+    TIME COMPLEXITY - O(N)
 '''
 class Solution:
     def secondMaxValueWithSorting(self, list):
@@ -43,16 +54,16 @@ class Solution:
                 return list[i-1]
         # return list[-2]
 
-    def secondMaxWithoutSorting(self, list):
+    def secondMaxDoubleTraversal(self, list):
         length = len(list)
         if not list or length == 1: # Testing edge cases.
             return None
         max_num = float('-inf')
         second_max_num = float('-inf')
-        for num in list:
+        for num in list: # Iterating through the list to find the max number.
             if num > max_num:
                 max_num = num
-        for i in range(length):
+        for i in range(length): # Iterating through the list to find the second max number which is not equal to the max number.
             if list[i] == max_num:
                 try:
                     i += 1
@@ -64,9 +75,27 @@ class Solution:
                     i += 1
         return second_max_num
 
+    def secondMaxSingleTraversal(self, list):
+        max_num = float('-inf')
+        second_max_num = float('-inf')
+
+        if not list or len(list) == 1: # Testing edge case.
+            return None
+
+        for i in range(len(list)):
+            if list[i] > max_num:
+                second_max_num = max_num
+                max_num = list[i]
+            elif (list[i] > second_max_num) and (list[i] != max_num):
+                    second_max_num = list[i]
+
+        return second_max_num
+
 
 if __name__ == '__main__':
     s = Solution()
     print(s.secondMaxValueWithSorting([9,9,-2,0,6]))
-    print(s.secondMaxWithoutSorting([1,-2,0,6,10,10]))
-    print(s.secondMaxWithoutSorting([1]))
+    print(s.secondMaxDoubleTraversal([1,-2,0,6,10,10]))
+    print(s.secondMaxDoubleTraversal([1]))
+    print(s.secondMaxSingleTraversal([1,-2,0,6,10,10]))
+    print(s.secondMaxSingleTraversal([1]))
