91f8c386af595ab2010f395c581616be04dcc383
setu-parekh <setuparekh@outlook.com>
2019-12-18 19:56:35 -0600

    General: find_max_depth_of_BT

---
+++ Trees/General/find_max_depth_of_BT.py
@@ -0,0 +1,93 @@
+'''
+QUESTION -
+----------
+Given the root to a Binary Search Tree, write a function to find the height of the tree.
+
+                6
+                |
+          -------------
+         |             |
+         4             9
+         |             |
+      -------       -------
+     |       |     |       |
+     2       5     8       12
+                           |
+                        -------
+                       |       |
+                       10      14
+
+Expected output: 4
+
+APPROACH -
+----------
+Recursive:
+    Base Case:
+        1. if node is none: return 0
+    Recursion Case:
+        1. Recursively call node.left and node.right simultaneously till base case is hit.
+        2. Find max out of node.left and node.right.
+        3. Add 1 to the max value and return
+
+TIME COMPLEXITY -
+-----------------
+O(N) as all nodes were visited.
+
+SPACE COMPLEXITY -
+------------------
+Unbalanced Tree: O(N)
+Balanced Tree: O(logN)
+'''
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self, root):
+        self.root = root
+
+    def insertNode(self, value):
+        newNode = Node(value)
+
+        if self.root is None:
+            self.root = newNode
+
+        else:
+            current = self.root
+            while True:
+                if value <= current.value:
+                    if current.left is None:
+                        current.left = newNode
+                        break
+                    else:
+                        current = current.left
+                elif value > current.value:
+                    if current.right is None:
+                        current.right = newNode
+                        break
+                    else:
+                        current = current.right
+
+    def findMaxDepth(self,node):
+        if node is None:
+            return 0
+        else:
+            return 1 + max(self.findMaxDepth(node.left), self.findMaxDepth(node.right))
+
+
+if __name__ == '__main__':
+    root = Node(6)
+    bst = BinarySearchTree(root)
+    bst.insertNode(4)
+    bst.insertNode(2)
+    bst.insertNode(5)
+    bst.insertNode(9)
+    bst.insertNode(12)
+    bst.insertNode(8)
+    bst.insertNode(10)
+    bst.insertNode(14)
+    print(bst.findMaxDepth(root))
