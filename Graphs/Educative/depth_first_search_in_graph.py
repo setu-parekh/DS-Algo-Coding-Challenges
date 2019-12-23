bac76bd1f073ae80d54f60ade450e0e56ca55d8d
setu-parekh <setuparekh@outlook.com>
2019-12-22 22:09:30 -0600

    Educative: depth_first_search_in_graph

--- Graphs/Educative/depth_first_search_in_graph.py
+++ Graphs/Educative/depth_first_search_in_graph.py
@@ -5,10 +5,17 @@ Write a program to perform depth first search of a graph from given list of edge
 
 APPROACH -
 -----------
-    1. Initialize graph as a defaultdict where vertices are the keys and list of their destination vertices as values.
-    2. Populate defaultdict with keys and values to create graph.
-    3. For DFS, create visited and result list.
-    4. For elements in the list of each key: append them to result and visited list, if that element is not already present in the visited list.
+Recursive:
+    1. For DFS, create visited and result list.
+    2. Starting from the given input vertex, dive deep down till we reach the deadend.
+    3. Backtrack from where the function call was made and explore any unexplored neighbour for that vertex.
+
+Iterative:
+    1. Initialize stack with start vertex.
+    2. Create visited list
+    3. Pop the vertex from the stack
+    4. Append vertex to visited list if that vertex is not already present. If present, then simply continue popping the next top vertex from the stack.
+    5. Push neighbours of popped vertex on to the stack.
 
 TIME COMPLEXITY -
 -----------------
@@ -31,10 +38,11 @@ class Solution:
 
         return self.graph
 
-    def dfs(self,start):
-        if start is None:
+    def dfsRecursive(self,start):
+        if start is None: # Edge Case
             return []
-        self.visited = []
+
+        self.visited = [] # Creating visited list to maintain the track of visited vertices so that they are not visited more than once.
         self.result = []
 
         def dfsHelper(node):
@@ -43,12 +51,33 @@ class Solution:
 
             for element in self.graph[node]:
                 if element not in self.visited:
-                    dfsHelper(element)
+                    dfsHelper(element) # Recursive call to the neighbour of the vertex
 
         dfsHelper(start)
         return self.result
 
+    def dfsIterative(self, start):
+        # Initialize stack with the start node
+        stack = [start]
+        # create empty visited list to keep the track of visited vertices so that they are not visited again.
+        visited = []
+        # Iterating till the stack is not empty:
+        while stack:
+            vertex = stack.pop()
+            # Appending the vertex to the visited list if not present already. If present, then continue popping next vertex.
+            if vertex not in visited:
+                visited.append(vertex)
+            else:
+                continue
+            # Pushing the neighbors of the vertex on to the stack.
+            for neighbor in self.graph[vertex]:
+                stack.append(neighbor)
+
+        return visited
+
 if __name__ == '__main__':
     s = Solution()
-    print(s.createAdjLst([[1,2],[1,3],[1,6],[2,4],[2,6],[3,5],[4,1],[4,3],[4,5],[6,4]]))
-    print(s.dfs(1))
+    # print(s.createAdjLst([[1,2],[1,3],[1,6],[2,4],[2,6],[3,5],[4,1],[4,3],[4,5],[6,4]]))
+    print(s.createAdjLst([[1,2],[1,3],[2,5],[2,4],[3,5],[4,6],[5,6],[6,7]]))
+    print(s.dfsRecursive(1))
+    print(s.dfsIterative(1))
